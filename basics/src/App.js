281b2c47aa0a3652e7bd5580b34eb4e204674aa6
PiotrSiwek <siwek.piotr@gmail.com>
2023-01-20 00:30:32 +0100

    Playing with props and components.

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App"></div>
-  );
+  return ( 
+    <div className="App">
+      <User name="Piotr" age={44} epost="ss@dd.dd" />
+      <User name="Pawel" age={55} epost="gg@dd.dd" />
+      <User name="Chris" age={66} epost="asd@dd.dd" />
+
+    </div>
+   );
 }
+
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.epost}</h1>
+    
+    </div>
+  );
+};
+
+
 
 export default App;
