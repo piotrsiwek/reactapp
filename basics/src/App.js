efabc0b2a41f585da7c62c3866f9d06b5e05de45
PiotrSiwek <siwek.piotr@gmail.com>
2023-01-21 00:21:10 +0100

    Reweriting to CSS modules

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,28 +1,12 @@
-import './App.css';
+import styles from "./App.module.css";
 
 function App() {
   return ( 
-    <div className="App">
-      <User name="Piotr" age={44} epost="ss@dd.dd" />
-      <User name="Pawel" age={55} epost="gg@dd.dd" />
-      <User name="Chris" age={66} epost="asd@dd.dd" />
-
+    <div className={styles.App}>   
+    <h1 className={styles.name}>sdfsdf</h1>
     </div>
    );
 }
-
-
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.epost}</h1>
-    
-    </div>
-  );
-};
-
 
 
 export default App;
